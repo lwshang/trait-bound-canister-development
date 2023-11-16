ad4b00fbdc3ecd7b1d981db3e64ac9a369c0c1e4
Linwei Shang <linwei.shang@dfinity.org>
2023-11-16 12:46:07 -0500

    docs: comments

--- src/lib.rs
+++ src/lib.rs
@@ -15,16 +15,20 @@ impl Counter for Canister {
     }
 
     fn canister_init(&mut self, arg0: Option<u64>) {
+        // set the counter with start value or default 0
         self.counter = arg0.unwrap_or(0);
     }
 
     fn canister_pre_upgrade(&mut self) {
+        // save the counter value into stable memory
         ic_cdk::storage::stable_save((self.counter,)).unwrap();
     }
 
     fn canister_post_upgrade(&mut self, arg0: Option<u64>) {
         match arg0 {
+            // set the counter with new start value
             Some(new_start) => self.counter = new_start,
+            // restore the counter value from stable memory
             None => {
                 let (previous,): (u64,) = ic_cdk::storage::stable_restore().unwrap();
                 self.counter = previous;
